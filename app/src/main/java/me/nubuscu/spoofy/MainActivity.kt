2ce967ee21c00bc3af748beedeefefe169a0458b
Tyler Kennedy <tke34@uclive.ac.nz>
2019-03-06 20:48:34 +1300

    Add very simple drawer to the main layout

--- app/src/main/java/me/nubuscu/spoofy/MainActivity.kt
+++ app/src/main/java/me/nubuscu/spoofy/MainActivity.kt
@@ -1,8 +1,14 @@
 package me.nubuscu.spoofy
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.MenuItem
 import kaaes.spotify.webapi.android.SpotifyApi
 import kaaes.spotify.webapi.android.SpotifyService
 import kaaes.spotify.webapi.android.models.Artist
@@ -14,6 +20,7 @@ import retrofit.client.Response
 class MainActivity : AppCompatActivity() {
     private lateinit var token: String
     private lateinit var spotify: SpotifyService
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,38 @@ class MainActivity : AppCompatActivity() {
         val api = SpotifyApi()
         api.setAccessToken(token)
         spotify = api.service
-        findArtist("Tesseract")
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     fun findArtist(artistName: String) {
